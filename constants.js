2ff495d9003356cf19e0c6f94e026aacc843aecd
jason2020 <jtay20@punahou.edu>
2022-06-20 19:50:11 -1000

    Started class adding option and cleaned up stuff

--- constants.js
+++ constants.js
@@ -1,22 +1,4 @@
 module.exports = {
-  urls: [
-    // "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=22F&subj_area_cd=EC%20ENGR&crs_catlg_no=0188%20%20%20%20&class_id=439828200&class_no=%20001%20%20",
-    "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=22F&subj_area_cd=EC%20ENGR&crs_catlg_no=0116C%20M%20&class_id=439398100&class_no=%20001%20%20",
-    "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=22F&subj_area_cd=COM%20SCI&crs_catlg_no=0131%20%20%20%20&class_id=187510200&class_no=%20001%20%20",
-    "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=22F&subj_area_cd=COM%20SCI&crs_catlg_no=0188%20%20%20%20&class_id=187827200&class_no=%20001%20%20",
-  ],
-
-  exchangeUrls: {
-    // "EC ENGR 188":
-    //   "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Computer+Science+(COM+SCI)&CrsCatlgName=131+-+Programming+Languages&t=22F&sBy=subject&subj=COM+SCI&catlg=0131&cls_no=%25&undefined=Go&IsClassExchange=true&dropClassId=187787202&btnIsInIndex=btn_inIndex",
-    "COM SCI 188":
-      "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Computer+Science+(COM+SCI)&CrsCatlgName=188+-+Natural+Language+Processing&t=22F&sBy=subject&subj=COM+SCI&catlg=0188&cls_no=+001++&undefined=Go&IsClassExchange=true&dropClassId=&btnIsInIndex=btn_inIndex",
-    "COM SCI 131":
-      "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Computer+Science+(COM+SCI)&CrsCatlgName=131+-+Programming+Languages&t=22F&sBy=subject&subj=COM+SCI&catlg=0131&cls_no=%25&undefined=Go&IsClassExchange=true&dropClassId=187787202&btnIsInIndex=btn_inIndex",
-    "EC ENGR M116C":
-      "https://sa.ucla.edu/ro/ClassSearch/Results?SubjectAreaName=Electrical+and+Computer+Engineering+(EC+ENGR)&CrsCatlgName=M116C+-+Computer+Systems+Architecture&t=22F&sBy=subject&subj=EC+ENGR&catlg=0116C+M&cls_no=%25&undefined=Go&IsClassExchange=true&dropClassId=&btnIsInIndex=btn_inIndex",
-  },
-
   // How often to check the SOC links (in microseconds)
-  freq: 1000 * 30,
+  freq: 1000 * 2,
 };
--- index.js
+++ index.js
@@ -1,70 +1,119 @@
 // Put the SOC links of the filled classes you want to watch here
-const { urls, freq, exchangeUrls } = require("./constants.js");
-const puppeteer = require("puppeteer");
-const open = require("open");
-const player = require("play-sound")((opts = {}));
-const notifier = require("node-notifier");
+const { freq } = require("./constants.js"),
+  puppeteer = require("puppeteer"),
+  open = require("open"),
+  player = require("play-sound")((opts = {})),
+  notifier = require("node-notifier"),
+  prompt = require("prompt"),
+  fs = require("fs");
 
-async function scrape(url) {
+const classesToTrack = JSON.parse(fs.readFileSync("./classes.json"));
+
+async function scrape(course) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(course[0]);
 
   // Get the "viewport" of the page, as reported by the page.
   const data = await page.evaluate(() => {
-    return {
-      status: document
-        .querySelector(
-          "#block-mainpagecontent > div > div > div > div > ucla-sa-soc-app"
-        )
-        .shadowRoot.querySelector(
-          "#enrl_mtng_info > table > tbody > tr > td:nth-child(1)"
-        ).innerHTML,
-      className: document
-        .querySelector(
-          "#block-mainpagecontent > div > div > div > div > ucla-sa-soc-app"
-        )
-        .shadowRoot.querySelector("#subject_class > p")
-        .innerHTML.split("<br>\n")[1]
-        .split(" - ")[0]
-        .trim(),
-    };
+    return document
+      .querySelector(
+        "#block-mainpagecontent > div > div > div > div > ucla-sa-soc-app"
+      )
+      .shadowRoot.querySelector(
+        "#enrl_mtng_info > table > tbody > tr > td:nth-child(1)"
+      )
+      .innerHTML.trim();
+    //   className: document
+    //     .querySelector(
+    //       "#block-mainpagecontent > div > div > div > div > ucla-sa-soc-app"
+    //     )
+    //     .shadowRoot.querySelector("#subject_class > p")
+    //     .innerHTML.split("<br>\n")[1]
+    //     .split(" - ")[0]
+    //     .trim(),
+    // };
   });
   await browser.close();
   return data;
 }
 
-async function checkFilledClassStatus(urls) {
-  let data = [];
-  for (let i = 0; i < urls.length; i++) {
-    const currData = await scrape(urls[i]);
-    if (currData.status.split(" ")[0] !== "Closed:")
-      data.push(currData.className);
-  }
-  return data;
-}
-
-async function main() {
-  setInterval(async () => {
-    const changedStatusClasses = await checkFilledClassStatus(urls);
-    changedStatusClasses.forEach((className) => {
-      console.log(`${className} is now open!`);
-      open(exchangeUrls[className]);
+async function notifyOnStatusChange() {
+  let state = false;
+  for (course in classesToTrack) {
+    const currData = await scrape(classesToTrack[course]);
+    if (currData.split(" ")[0] !== "Closed:") {
+      console.log(`${course} is now open!`);
+      open(classesToTrack[course][1]);
       player.play("alertSound.mp3", function (err) {
         if (err) throw err;
       });
       notifier.notify({
         title: "CLASS OPENED",
-        message: `${className} is now open!`,
+        message: `${course} is now open!`,
       });
-    });
+      state = true;
+    }
+  }
+  console.log(
+    state
+      ? "Classes status changed"
+      : `No status changed ${new Date().toLocaleString()}`
+  );
+}
 
-    console.log(
-      changedStatusClasses.length === 0
-        ? `No status changed ${new Date().toLocaleString()}`
-        : "Classes status changed"
-    );
-  }, freq);
+function enterNewClasses() {
+  const schema = {
+    properties: {
+      subjectArea: {
+        description: "Subject Area (e.g. COM SCI)",
+        type: "string",
+        required: true,
+      },
+      className: {
+        description: "Class Name (e.g. 111)",
+        type: "string",
+        required: true,
+      },
+    },
+  };
+  prompt.get(schema, function (err, result) {
+    const fullName = `${result.subjectArea} ${result.className}`;
+    let currData = JSON.parse(fs.readFileSync("./classes.json"));
+    if (currData.hasOwnProperty(fullName)) {
+      console.log("Class already exists");
+      main();
+    }
+    console.log("Retrieving class info...");
+  });
+}
+
+async function main() {
+  prompt.start();
+  const schema = {
+    properties: {
+      runState: {
+        description: "Run enrollment checker? (y/n)",
+        type: "string",
+        pattern: /^(y|n|Y|N)$/,
+        required: true,
+        default: "y",
+      },
+    },
+  };
+
+  prompt.get(schema, function (err, result) {
+    let runState = result.runState.toLocaleLowerCase();
+    if (runState === "y") {
+      console.clear();
+      console.log("Enrollment checker started");
+      setInterval(notifyOnStatusChange, freq);
+    } else if (runState === "n") {
+      console.clear();
+      console.log("Entering enrollment configuration mode");
+      enterNewClasses();
+    }
+  });
 }
 
 main();
