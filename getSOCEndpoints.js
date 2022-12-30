157c28fccf5d82eb28dfc4855223e06f13bebad7
jason2020 <jtay20@punahou.edu>
2022-12-29 21:59:22 -1000

    Testing SOC endpoint structure

--- getSOCEndpoints.js
+++ getSOCEndpoints.js
@@ -109,3 +109,16 @@ async function getSubjectAreas(browser, SOCPage, quarter) {
   await page.close();
   return subjectAreas
 }
+
+// For experimenting with the SOC endpoints to figure out if there's a way to generate the links without scraping the SOC website
+function SOCexperimenting() {
+  // https://sa.ucla.edu/ro/public/soc/Results/GetCourseSummary?model=%7B%22Term%22%3A%2223W%22%2C%22SubjectAreaCode%22%3A%22AERO+ST%22%2C%22CatalogNumber%22%3A%220000A+++%22%2C%22IsRoot%22%3Atrue%2C%22SessionGroup%22%3A%22%25%22%2C%22ClassNumber%22%3A%22%25%22%2C%22SequenceNumber%22%3Anull%2C%22Path%22%3A%22AEROST0000A%22%2C%22MultiListedClassFlag%22%3A%22n%22%2C%22Token%22%3A%22MDAwMEEgICBBRVJPU1QwMDAwQQ%3D%3D%22%7D
+  const params = new URLSearchParams('?model=%7B%22Term%22%3A%2223W%22%2C%22SubjectAreaCode%22%3A%22AERO+ST%22%2C%22CatalogNumber%22%3A%220000A+++%22%2C%22IsRoot%22%3Atrue%2C%22SessionGroup%22%3A%22%25%22%2C%22ClassNumber%22%3A%22%25%22%2C%22SequenceNumber%22%3Anull%2C%22Path%22%3A%22AEROST0000A%22%2C%22MultiListedClassFlag%22%3A%22n%22%2C%22Token%22%3A%22MDAwMEEgICBBRVJPU1QwMDAwQQ%3D%3D%22%7D&FilterFlags=%7B%22enrollment_status%22%3A%22O%2CW%2CC%2CX%2CT%2CS%22%2C%22advanced%22%3A%22y%22%2C%22meet_days%22%3A%22T%2CF%22%2C%22start_time%22%3A%227%3A00+am%22%2C%22end_time%22%3A%221%3A00+pm%22%2C%22meet_locations%22%3Anull%2C%22meet_units%22%3Anull%2C%22instructor%22%3Anull%2C%22class_career%22%3Anull%2C%22impacted%22%3A%22N%22%2C%22enrollment_restrictions%22%3Anull%2C%22enforced_requisites%22%3Anull%2C%22individual_studies%22%3A%22n%22%2C%22summer_session%22%3Anull%7D')
+  const options = Object.fromEntries(params.entries())
+
+  console.log(options);
+
+  for (const [key, value] of Object.entries(options)) {
+    console.log(JSON.parse(value));
+  }
+}
