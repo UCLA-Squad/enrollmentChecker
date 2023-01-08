18953f182e649b0f8f22620fa81d030d220e395e
RogueArt <altoraries@gmail.com>
2023-01-07 21:00:57 -0800

    Add a parser function to parse data for a given lecture

--- src/parser.js
+++ src/parser.js
@@ -24,3 +24,78 @@ async function parseFromSOCURL(SOCURL) {
 
   return { classID, internalIDs, classesInfo }
 }
+
+/**
+ * Example: Select Computer Science (COM SCI) 31 - Introduction to Computer Science I Lec 1
+ * We ignore the "Select" part of this
+ * Group 1 - Match the department name - Computer Science
+ * Group 2 - Match the short departent name - COM SCI
+ * Group 3 - Match the course number in series - 31
+ * Group 4 - Match the course description - Introduction to Computer Science I
+ * Group 5 - Match the lecture section name
+ */
+const COURSE_NAME_REGEX =
+  /Select ([\w\s]+) \(([\w\s]+)\)\s+([\w\d]+)\s+-\s+([\w\s]+) ((Lec|Lab) (\d+))/
+
+async function parseElem($, e, internalId) {
+  // Get if it's open & spots left
+  const statusText = $(e).find(`div#${internalId}-status_data`).text().trim()
+  const isOpen = statusText.includes('Open')
+  const isWaitlist = statusText.includes('Waitlist')
+  const isClosed = statusText.includes('Closed')
+  if (isOpen) {
+    var [openSeats, totalSeats]  = statusText.match(/(\d+) of (\d+)/)?.slice(1)
+  }
+  else if (isWaitlist) {
+    var [totalSeats] = statusText.match(/Class Full \((\d+)\)/)?.slice(1)
+    const waitlistText = $(`div#${internalId}-waitlist_data`).text().trim()
+    var [takenWaitlistSeats, totalWaitlistSeats] = waitlistText.match(/(\d+) of (\d+) Taken/)?.slice(1)
+  }
+  else if (isClosed) {
+    var [totalSeats] = statusText.match(/Class Full \((\d+)\)/)?.slice(1)
+  }
+
+  // Get waitlist status
+  const waitlistText = $(e).find(`div#${internalId}-waitlist_data`).text()
+  const isWaitlisted = !waitlistText.includes('No') && isWaitlist
+
+  // Get days, time, location, units, and instructors
+  const meetingDays = $(e).find(`div#${internalId}-days_data > p > button`).first().text().trim().split('')
+  const meetingTime = $(e).find(`div#${internalId}-time_data > p`).text().trim()
+  const location = $(e).find(`div#${internalId}-location_data`).text().trim()
+  const units = $(e).find(`div#${internalId}-units_data`).text().trim()
+  const instructors = $(e).find(`div#${internalId}-instructor_data`).text().trim()
+
+  // Get course name information
+  const courseFullNameInfo = $(e).find('.screenReaderOnly').text().trim()
+  const [
+    _,
+    // courseFullNameInfo,
+    courseLongCategory,
+    courseShortCategory,
+    courseNumber,
+    courseDescription,
+    lectureSection,
+  ] = courseFullNameInfo.match(COURSE_NAME_REGEX)
+
+  return {  
+    isOpen,
+    isWaitlisted,
+    isClosed,
+    openSeats: +(openSeats ?? 0),
+    totalSeats: +(totalSeats ?? 0),
+    takenWaitlistSeats: takenWaitlistSeats ? +takenWaitlistSeats : null,
+    totalWaitlistSeats: totalWaitlistSeats ? +totalWaitlistSeats : null,
+    courseLongCategory,
+    courseShortCategory,
+    courseNumber,
+    courseShortName: courseShortCategory + ' ' + courseNumber,
+    courseLongName: courseDescription,
+    lectureSection,
+    meetingDays,
+    meetingTime,
+    location,
+    units,
+    instructors,
+  }
+}
