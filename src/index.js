ffb7ad3bedadb08e23314225a8498b97237401cb
jason2020 <jtay20@punahou.edu>
2023-01-14 18:36:55 -0800

    Capture classes one at a time, documented errors in issue

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const { CHROME_PROFILE_DIR } = process.env
 // TO-DO: Use a logging library!
 async function main() {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: [`--user-data-dir=${CHROME_PROFILE_DIR}`],
   })
 
@@ -25,16 +25,35 @@ async function main() {
 
   console.info('Capturing HTTP requests from all subject areas WITH pagination')
   const output = []
-  for (let x = 0; x < subjectAreaURLs.length + 4; x += 5) {
-    const nPromises = subjectAreaURLs
-      .slice(x, x + 5)
-      .map(async subjectAreaURL => captureWithPagination(browser, subjectAreaURL))
-    const nCaptures = await Promise.all(nPromises).catch(e => console.log(e))
-    output.push(...(nCaptures ?? []))
-    console.info(`Finished processing (${x+5}/${subjectAreaURLs.length})`)
-  }
+  // for (let x = 0; x < subjectAreaURLs.length + 4; x += 5) {
+  //   const nPromises = subjectAreaURLs
+  //     .slice(x, x + 5)
+  //     .map(async subjectAreaURL => captureWithPagination(browser, subjectAreaURL))
+  //   const nCaptures = await Promise.all(nPromises).catch(e => console.log(e))
+  //   output.push(...(nCaptures ?? []))
+  //   console.info(`Finished processing (${x + 5}/${subjectAreaURLs.length})`)
+  //   let oldData = [];
+  //   try {
+  //     oldData = JSON.parse(fs.readFileSync(PATHS.OUTPUT_FILE, 'utf8'));
+  //   } catch (e) { }
+  //   fs.writeFileSync(PATHS.OUTPUT_FILE, JSON.stringify([...oldData, ...output], null, 2), 'utf8');
+  // }
 
-  fs.writeFileSync(PATHS.OUTPUT_FILE, JSON.stringify(output, null, 2), 'utf8')
+  for (let x = 25; x < subjectAreaURLs.length; x++) {
+    try {
+      const subjectAreaURL = subjectAreaURLs[x]
+      const nCaptures = await captureWithPagination(browser, subjectAreaURL)
+      output.push(...(nCaptures ?? []))
+      console.info(`Finished processing (${x + 1}/${subjectAreaURLs.length})`)
+      let oldData = [];
+      try {
+        oldData = JSON.parse(fs.readFileSync(PATHS.OUTPUT_FILE, 'utf8'));
+      } catch (e) { }
+      fs.writeFileSync(PATHS.OUTPUT_FILE, JSON.stringify([...oldData, ...output], null, 2), 'utf8');
+    } catch (e) {
+      console.log(`Failed ${subjectAreaURLs[x]}}`);
+    }
+  }
 
   await browser.close()
 }
